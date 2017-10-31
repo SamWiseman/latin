8c769cc23ad8bb63ce359deb10b9f21540b2ebe2
spoonybard <rukaruth@gmail.com>
2017-10-31 15:38:02 -0500

    refactoring CorpusData class (this draft puts calculate probabilities in the class, but we could also imagine arguments for having it outside the class)

--- LDA.py
+++ LDA.py
@@ -4,45 +4,25 @@ import csv
 import random
 from numpy.random import choice
 
-# global data structures (see pseudoLDA.py for explanations)
-#TODO: take this out
-wordsByLocation = []
-topicsByLocation = []
-individualWordList = []
-wordTopicCounts = []
-wordCounts = []
-topicList = []
-topicWordCounts = []
-docList = []
-docWordCounts = []
-
 """
-printTopics() -- this method prints each topic in topicList on a new line in the format 
-"Topic 1: word1, word2, word3, etc." The words are sorted according to highest incidence 
-in the topic.
-"""
-#TODO: put this in the class
-#TODO: print to file instead of to command line
-def printTopics():
-    for topic in topicList:
-        print("Topic " + str(topicList.index(topic)+1) + ": "),
-        print(", ".join(sorted(topic, key=topic.get, reverse=True)))
-
-
-"""
-runLDA(iterations, alpha, beta) -- this method handles the iteration of LDA, calling helper methods
+runLDA(iterations, file, topics, alpha, beta) -- this method handles the iteration of LDA, calling helper methods
 for each iteration and printing at the end.
 :param iterations: int -- number of iterations to run
+:param file: string -- file name to read data from
+:param topics: int -- how many topics to create
 :param alpha: float -- hyperparameter to add to each P(w|t)
 :param beta: float -- hyperparameter to add to each P(t|d)
 """
-def runLDA(iterations, alpha, beta):
+def runLDA(iterations, file, topics, alpha, beta):
+    corpus = CorpusData(file, topics)
     for i in range(0, iterations):
-        for doc in wordsByLocation:
+        for doc in corpus.wordsByLocation:
             for word in doc:
-                wordProbabilities = calculateProbabilities(doc, word)
-                updateDataStructures(word, doc, wordProbabilities)
-    printTopics()
+                wordProbabilities = corpus.calculateProbabilities(doc, word, alpha, beta)
+                oldTopic = corpus.topicsByLocation[doc][word]
+                newTopic = choice(range(0, len(wordProbabilities)), p=wordProbabilities)
+                corpus.updateDataStructures(word, doc, oldTopic, newTopic)
+    corpus.printTopics()
 
 class CorpusData:
     # Location Information
@@ -165,29 +145,45 @@ class CorpusData:
                 self.topicWordCounts[i] += wordTopics[i]
                 #if wordTopics[i] != 0:
                 #    self.topicWordCounts[i] += 1
-            
-        
-#test function for data loading
-def loadTest():
-    data = CorpusData('wiki5Docs.csv', 10)
-    data.loadData()
+
+    """
+    printTopics() -- this method prints each topic in topicList on a new line in the format 
+    "Topic 1: word1, word2, word3, etc." The words are sorted according to highest incidence 
+    in the topic.
+    """
+
+    # TODO: print to file instead of to command line
+    def printTopics(self):
+        for topic in self.topicList:
+            print("Topic " + str(self.topicList.index(topic) + 1) + ": "),
+            print(", ".join(sorted(topic, key=topic.get, reverse=True)))
+
+    def updateDataStructures(self, word, doc, oldTopic, newTopic):
+        wordString = self.wordsByLocation[doc][word]
+        self.topicsByLocation[doc][word] = newTopic
+        self.topicList[oldTopic][wordString] -= 1
+        self.topicList[newTopic][wordString] += 1
+        self.topicWordCounts[oldTopic] -= 1
+        self.topicWordCounts[newTopic] += 1
+        self.docList[doc][oldTopic] -= 1
+        self.docList[doc][newTopic] += 1
     
-''' LDA methods for recalculating the probabilities of each word by topic '''
-#TODO: hyperparameters
-def calculateProbabilities(docCoord, wordCoord):
-    word = wordsByLocation[docCoord][wordCoord]
-    newWordProbs = []
-    for i in range(len(topicList)):
-        # pwt = P(w|t)
-        topicDict = topicList[i]
-        wordCount = topicDict[word]
-        pwt = wordCount / topicWordCounts[i]
-        # ptd = P(t|d)
-        wordsInTopicInDoc = docList[docCoord][i]
-        ptd = wordsInTopicInDoc / docWordCounts[docCoord]
-        # ptw = P(t|w)
-        ptw = pwt * ptd
-        newWordProbs.append(ptw)
+    ''' LDA methods for recalculating the probabilities of each word by topic '''
+    #TODO: hyperparameter inclusion in calculations
+    def calculateProbabilities(self, docCoord, wordCoord, alpha, beta):
+        word = self.wordsByLocation[docCoord][wordCoord]
+        newWordProbs = []
+        for i in range(len(self.topicList)):
+            # pwt = P(w|t)
+            topicDict = self.topicList[i]
+            wordCount = topicDict[word]
+            pwt = wordCount / self.topicWordCounts[i]
+            # ptd = P(t|d)
+            wordsInTopicInDoc = self.docList[docCoord][i]
+            ptd = wordsInTopicInDoc / self.docWordCounts[docCoord]
+            # ptw = P(t|w)
+            ptw = pwt * ptd
+            newWordProbs.append(ptw)
         #TODO: once we get hyperparameters involved, will we need to re-regularize here?
         '''
         example:
@@ -198,32 +194,5 @@ def calculateProbabilities(docCoord, wordCoord):
             regularProbabilities.append(probability/one)
         return regularProbabilities
         '''
-    return newWordProbs
+        return newWordProbs
 
-
-"""
-updateDataStructures(word, doc, wordProbabilities) -- this method chooses a new topic assignment for the 
-given instance of the word based on its calculated topic probabilities and updates all relevant data 
-structures to change its assignment
-
-:param word: int -- word index in location 2D arrays
-:param doc: int -- doc index in location 2D arrays
-:param wordProbabilities: list -- probabilities of word in each topic
-:return: none
-@TODO: use class-based update method for topic reassignment
-"""
-def updateDataStructures(word, doc, wordProbabilities):
-    wordString = wordsByLocation[doc][word]
-    oldTopic = topicsByLocation[doc][word]
-
-    newTopic = choice(range(0, len(wordProbabilities)), p=wordProbabilities)
-    topicsByLocation[doc][word] = newTopic
-
-    topicList[oldTopic][wordString] -= 1
-    topicList[newTopic][wordString] += 1
-
-    topicWordCounts[oldTopic] -= 1
-    topicWordCounts[newTopic] += 1
-
-    docList[doc][oldTopic] -= 1
-    docList[doc][newTopic] += 1
