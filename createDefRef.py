08c272f26e28e6ff085a7f8f79029388396c1249
Martha <durrettm@carleton.edu>
2018-01-07 14:51:18 -0600

    sorts words in topic by frequency for output.csv

--- createDefRef.py
+++ createDefRef.py
@@ -7,20 +7,24 @@ import xlrd
 workbook = xlrd.open_workbook('latinDefinitions.xlsx', on_demand = True)
 worksheet = workbook.sheet_by_name('Sheet1')
 
-dictionaryDict = {}
-for i in range(worksheet.nrows()):
-    dictionaryDict[worksheet.cell(0, i+2).value] = i+2
+#dictionaryDict holds the location of the definitions in the latinDefinitions spreadsheet for each word
 
-defsForCSV = []
-with open('output.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in reader:
-        #get location in spreadsheet
-        defLocation = dictionaryDict[row[0]]
-        definition = worksheet.cell(0, defLocation)
-        defsForCSV.append([row[0], definition])
+def makeDefRefs(numTopics):
+    dictionaryDict = {}
+    for i in range(worksheet.nrows()):
+        dictionaryDict[worksheet.cell(0, i+2).value] = i+2
 
-with open('defRefs.csv', 'w', newline='') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for row in defsForCSV:
-        filewriter.writerow(row)
+    defsForCSV = []
+    with open('output.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for i in range(numTopics):
+            for row in reader:
+                #get location in spreadsheet
+                defLocation = dictionaryDict[row[3*i]]
+                definition = worksheet.cell(0, defLocation)
+                defsForCSV.append([row[3*i], definition])
+
+    with open('defRefs.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in defsForCSV:
+            filewriter.writerow(row)
