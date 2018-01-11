1b73ed20894a990fffd11f894d65a574ae05e894
EstelleEvelyn <estelle.bayer@gmail.com>
2018-01-11 10:53:25 -0600

    Restructured parts of evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,34 +1,33 @@
 import numpy as np
 
-#
+#Determine the variance in topic size for the whole document
+#(Are topics all similarly sized?)
 def compareTopicSize(LDAModel):
     with open('eval.txt', 'a') as eval:
-        print(LDAModel.docTopicalWordDist)
-        for i in range(len(LDAModel.docTopicalWordDist)):
-            print(np.var(list(map(lambda x: x/sum(LDAModel.docTopicalWordDist[i])), LDAModel.docTopicalWordDist)))
-            eval.write("Topic size variance: " + np.var(list(map(lambda x: x/sum(LDAModel.docTopicalWordDist[i]), LDAModel.docTopicalWordDist[i]))) + "\n")
+        print(LDAModel.topicTotalWordCount)
+        for i in range(len(LDAModel.topicTotalWordCount)):
+            #print the variance of the word distribution into topics list
+            eval.write("Topic size variance: " + np.var(LDAModel.topicTotalWordCount) + "\n")
             eval.write("\n")
 
-
+#Compare the distribution of topics in the entire corpus to that of indivdual documents
+#(Do the documents have distinctive distributions, or just mirror the whole?)
 def compareDistributions(LDAModel):
-    wholeCorpList = []
-    for document in LDAModel.docTopicalWordDist:
-        for i in range(len(document)):
-            wholeCorpList[i] = wholeCorpList[i] + document[i]
-    totalWordsTopic = sum(wholeCorpList)
-    wholeCorpList = list(map(lambda x: x/totalWordsTopic, wholeCorpList))
     with open('eval.txt', 'a') as eval:
-        eval.write("Whole corpus distribution: "+ str(wholeCorpList))
+        eval.write("Whole corpus distribution: "+ list(map(lambda x: x/sum(LDAModel.topicTotalWordCount), LDAModel.topicTotalWordCount)))
         i = 0
+        #print topic distributions for each document as percentage lists
         for document in LDAModel.docTopicalWordDist:
             document = list(map(lambda x: x/sum(document), document))
-            eval.write("Document " + i + " distribution: " + str(document) + "\n")
+            eval.write("Document " + i + " distribution: " + document + "\n")
             i += 1
         eval.write("\n")
 
-
+#Compare a topic's size to its prevalence
+#(More specific topics should appear in larger proportions of a few documents)
 def topicSpecificity(LDAModel):
     topics = []
+    #generate an
     for i in range(len(LDAModel.topicList)):
         topics[i] = []
     for j in range(len(LDAModel.docTopicalWordDist)):
